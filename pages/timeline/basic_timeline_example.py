f844103f787198948e15528e294c78a8fa6c9892
pip-install-python <pipinstallpython@gmail.com>
2024-03-03 11:16:54 -0600

    cleaned up basic_timeline_example.py code examples.

--- pages/timeline/basic_timeline_example.py
+++ pages/timeline/basic_timeline_example.py
@@ -245,32 +245,32 @@ def return_timeline(
                                                             [
                                                                 dmc.Prism(
                                                                     """
-                    import dash
-                    from dash import html
-                    from flask import Flask
-            
-                    server = Flask(__name__)
-            
-                    app = dash.Dash(__name__,
-                    external_stylesheets=[
-                        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
-                        'https://unpkg.com/vis-timeline@latest/styles/vis-timeline-graph2d.min.css'
-                    ],
-                    external_scripts=[
-                        'https://unpkg.com/vis-timeline@latest/standalone/umd/vis-timeline-graph2d.min.js',
-                    ],
-                    assets_url_path="assets",
-                    use_pages=True,
-                    server=server)
-            
-                    layout = html.Div([
-                        html.Div(id='visualization',
-                                         style={'width': '100%', 'height': '100%'}),
-                        DeferScript(src='assets/timeline/vistimeline.js'),
-                    ])
-            
-                    if __name__ == "__main__":
-                        app.run_server(port=43932, debug=True)
+import dash
+from dash import html
+from flask import Flask
+
+server = Flask(__name__)
+
+app = dash.Dash(__name__,
+external_stylesheets=[
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+    'https://unpkg.com/vis-timeline@latest/styles/vis-timeline-graph2d.min.css'
+],
+external_scripts=[
+    'https://unpkg.com/vis-timeline@latest/standalone/umd/vis-timeline-graph2d.min.js',
+],
+assets_url_path="assets",
+use_pages=True,
+server=server)
+
+layout = html.Div([
+    html.Div(id='visualization',
+                     style={'width': '100%', 'height': '100%'}),
+    DeferScript(src='assets/timeline/vistimeline.js'),
+])
+
+if __name__ == "__main__":
+    app.run_server(port=43932, debug=True)
                                                                 """,
                                                                     language="python",
                                                                     colorScheme="dark",
@@ -376,57 +376,57 @@ def return_timeline(
                                                             [
                                                                 dmc.Prism(
                                                                     """
-                                                                var container = document.getElementById("visualization");
-            
-                                                                // note that months are zero-based in the JavaScript Date object
-                                                                var items = new vis.DataSet([
-                                                                  {
-                                                                    start: new Date(2024, 7, 23),
-                                                                    content:
-                                                                      '<div>Conversation</div><img src="https://icons.iconarchive.com/icons/iconfactory/looney/32/Madador-B-icon.png" style="width:32px; height:32px;">',
-                                                                  },
-                                                                  {
-                                                                    start: new Date(2024, 7, 23, 23, 0, 0),
-                                                                    content:
-                                                                      '<div>Mail from boss</div><img src="https://cdn.wikimg.net/en/strategywiki/images/4/40/WD_Babasama.gif" style="width:32px; height:32px;">',
-                                                                  },
-                                                                  { start: new Date(2024, 7, 24, 16, 0, 0), content: "Report" },
-                                                                  {
-                                                                    start: new Date(2024, 7, 26),
-                                                                    end: new Date(2024, 8, 2),
-                                                                    content: "Traject A",
-                                                                  },
-                                                                  {
-                                                                    start: new Date(2024, 7, 28),
-                                                                    content:
-                                                                      '<div>Memo</div><img src="https://gadgetsin.com/uploads/2021/03/divoom_pixoo_max_pixel_led_display_1-66x66.jpg" style="width:48px; height:48px;">',
-                                                                  },
-                                                                  {
-                                                                    start: new Date(2024, 7, 29),
-                                                                    content:
-                                                                      '<div>Phone call</div><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.iconsdb.com%2Ficons%2Fpreview%2Fdim-gray%2Fphone-32-xl.png&f=1&nofb=1&ipt=dde3425f8a9454a0655ad8e4a21aaf30c64fcc548ab7c3b6c414b6b088f635a3&ipo=images" style="width:32px; height:32px;">',
-                                                                  },
-                                                                  {
-                                                                    start: new Date(2024, 7, 31),
-                                                                    end: new Date(2024, 8, 3),
-                                                                    content: "Traject B",
-                                                                  },
-                                                                  {
-                                                                    start: new Date(2024, 8, 4, 12, 0, 0),
-                                                                    content:
-                                                                      '<div>Report</div><img src="hhttp://127.0.0.1:43932/assets/timeline/imgs/duck.png" style="width:32px; height:32px;">',
-                                                                  },
-                                                                ]);
-            
-                                                                var options = {
-                                                                  editable: true,
-                                                                  margin: {
-                                                                    item: 20,
-                                                                    axis: 40,
-                                                                  },
-                                                                };
-            
-                                                                var timeline = new vis.Timeline(container, items, options);
+var container = document.getElementById("visualization");
+
+// note that months are zero-based in the JavaScript Date object
+var items = new vis.DataSet([
+  {
+    start: new Date(2024, 7, 23),
+    content:
+      '<div>Conversation</div><img src="https://icons.iconarchive.com/icons/iconfactory/looney/32/Madador-B-icon.png" style="width:32px; height:32px;">',
+  },
+  {
+    start: new Date(2024, 7, 23, 23, 0, 0),
+    content:
+      '<div>Mail from boss</div><img src="https://cdn.wikimg.net/en/strategywiki/images/4/40/WD_Babasama.gif" style="width:32px; height:32px;">',
+  },
+  { start: new Date(2024, 7, 24, 16, 0, 0), content: "Report" },
+  {
+    start: new Date(2024, 7, 26),
+    end: new Date(2024, 8, 2),
+    content: "Traject A",
+  },
+  {
+    start: new Date(2024, 7, 28),
+    content:
+      '<div>Memo</div><img src="https://gadgetsin.com/uploads/2021/03/divoom_pixoo_max_pixel_led_display_1-66x66.jpg" style="width:48px; height:48px;">',
+  },
+  {
+    start: new Date(2024, 7, 29),
+    content:
+      '<div>Phone call</div><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.iconsdb.com%2Ficons%2Fpreview%2Fdim-gray%2Fphone-32-xl.png&f=1&nofb=1&ipt=dde3425f8a9454a0655ad8e4a21aaf30c64fcc548ab7c3b6c414b6b088f635a3&ipo=images" style="width:32px; height:32px;">',
+  },
+  {
+    start: new Date(2024, 7, 31),
+    end: new Date(2024, 8, 3),
+    content: "Traject B",
+  },
+  {
+    start: new Date(2024, 8, 4, 12, 0, 0),
+    content:
+      '<div>Report</div><img src="hhttp://127.0.0.1:43932/assets/timeline/imgs/duck.png" style="width:32px; height:32px;">',
+  },
+]);
+
+var options = {
+  editable: true,
+  margin: {
+    item: 20,
+    axis: 40,
+  },
+};
+
+var timeline = new vis.Timeline(container, items, options);
                                                                 """,
                                                                     language="javascript",
                                                                     colorScheme="dark",
